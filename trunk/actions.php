74e4f158d632c0a020a742a73722a1ede1901233
jmschillaci <jmschillaci@2e201742-8e54-11dd-8a42-c3ad82d480ce>
2008-10-01 18:30:37 +0000

    agregado con gears pero todavia no me functiona bien la parte del getactions (habria que ver bien el getElementBytagname(actions)

---
+++ trunk/actions.php
@@ -0,0 +1,25 @@
+<?php
+ require_once("DB.php");
+
+header(  "content-type: text/xml" );
+
+$db =& DB::Connect(  'mysql://root:lanux@localhost/gtdgears', array() );
+if (PEAR::isError($db))  { die($db->getMessage()); }
+
+$res = $db->query(  'SELECT * FROM actions' );
+?>
+<actions id="actions">
+<?php
+while(  $res->fetchInto( $row ) ) {
+?>
+<action  id="<?php echo($row[0]); ?>" buc_id="<?php echo($row[1]); ?>"
+name="<?php  echo($row[2]); ?>" 
+startdate="<?php  echo($row[4]); ?>"  
+enddate="<?php  echo($row[4]);?>">
+
+<?php echo($row[3]);  ?>
+</action>
+<?php
+}
+?>
+</actions>
