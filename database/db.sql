06556bbdc8ccdd3ae538293cdfe6961cb3de98f4
jmschillaci <jmschillaci@2e201742-8e54-11dd-8a42-c3ad82d480ce>
2008-10-03 19:48:03 +0000

    Agregado context id a la tabla actions

--- database/db.sql
+++ database/db.sql
@@ -25,12 +25,13 @@ SET character_set_client = utf8;
 CREATE TABLE `actions` (
   `act_id` int(10) unsigned NOT NULL auto_increment,
   `act_buc_id` int(11) NOT NULL,
+  `act_ctxt_id` int(11) NOT NULL,
   `act_name` varchar(255) default NULL,
   `act_descr` text,
   `act_startdate` datetime NOT NULL,
   `act_enddate` datetime NOT NULL,
   PRIMARY KEY  (`act_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -39,7 +40,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `actions` WRITE;
 /*!40000 ALTER TABLE `actions` DISABLE KEYS */;
-INSERT INTO `actions` VALUES (1,1,'action1','hacer cosa 1','2008-10-01 13:47:13','2008-10-01 13:47:13'),(2,1,'action2','hacercosa2','2008-10-01 13:47:21','2008-10-01 13:47:21'),(3,1,'action3','hacer cosa3','2008-10-01 13:47:28','2008-10-01 13:47:28'),(4,1,'action3','hacer cosa3','2008-10-01 13:56:40','2008-10-01 13:56:40'),(5,1,'action3','hacer cosa3','2008-10-01 15:17:57','2008-10-01 15:17:57'),(6,1,'action3','hacer cosa3','2008-10-01 15:22:03','2008-10-01 15:22:03');
+INSERT INTO `actions` VALUES (1,1,1,'action1','hacer cosa 1','2008-10-01 13:47:13','2008-10-01 13:47:13'),(2,1,1,'action2','hacercosa2','2008-10-01 13:47:21','2008-10-01 13:47:21'),(3,1,2,'action3','hacer cosa3','2008-10-01 13:47:28','2008-10-01 13:47:28'),(4,1,3,'action4','hacer cosa3','2008-10-01 13:56:40','2008-10-01 13:56:40'),(5,1,1,'action5','hacer cosa3','2008-10-01 15:17:57','2008-10-01 15:17:57'),(6,1,2,'action6','hacer cosa3','2008-10-01 15:22:03','2008-10-01 15:22:03'),(8,1,1,'action4','action 4 para hacer','2008-10-03 14:56:41','2008-10-03 14:56:41'),(10,2,2,'accion 7','probar','2008-10-03 14:58:36','2008-10-03 14:58:36');
 /*!40000 ALTER TABLE `actions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +56,7 @@ CREATE TABLE `contexts` (
   `ctxt_name` varchar(50) NOT NULL,
   `ctxt_desc` varchar(255) NOT NULL,
   PRIMARY KEY  (`ctxt_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -64,6 +65,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `contexts` WRITE;
 /*!40000 ALTER TABLE `contexts` DISABLE KEYS */;
+INSERT INTO `contexts` VALUES (3,'Lanux','Cosas de Lanux'),(2,'MW','Cosas de Laburo'),(1,'Personal','Cosas personales');
 /*!40000 ALTER TABLE `contexts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-10-01 18:30:04
+-- Dump completed on 2008-10-03 19:48:08
