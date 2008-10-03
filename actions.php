52c459aae2534c09c56a648ecf40f96f494f0067
jmschillaci <jmschillaci@2e201742-8e54-11dd-8a42-c3ad82d480ce>
2008-10-03 19:47:36 +0000

    Agregada la pagina principal

--- actions.php
+++ actions.php
@@ -11,13 +11,7 @@ $res = $db->query(  'SELECT * FROM actions' );
 <?php
 while(  $res->fetchInto( $row ) ) {
 ?>
-<action  id="<?php echo($row[0]); ?>" buc_id="<?php echo($row[1]); ?>"
-name="<?php  echo($row[2]); ?>" 
-startdate="<?php  echo($row[4]); ?>"  
-enddate="<?php  echo($row[4]);?>">
-
-<?php echo($row[3]);  ?>
-</action>
+<action  id="<?php echo($row[0]);?>" buc_id="<?php echo($row[1]);?>"  ctxt_id="<?php echo($row[2]);?>" name="<?php  echo($row[3]);?>" startdate="<?php  echo($row[5]); ?>" enddate="<?php  echo($row[6]);?>"><?php echo($row[4]);?></action>
 <?php
 }
 ?>
